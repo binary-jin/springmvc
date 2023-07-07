0b52db08cab93bb1b31e06cafcf97d788b5ac6f0
yi-jin <dlwls4338@naver.com>
2023-07-07 15:16:44 +0900

    요청 파라미터 -modelAttribute 실습

--- src/main/java/hello/springmvc/basic/HelloData.java
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -1,4 +1,9 @@
 package hello.springmvc.basic;
 
+import lombok.Data;
+
+@Data //getter, setter, tostring, equalhashcode, requiredargsconstructor 를 자동 생성해줌
 public class HelloData {
+    private String username;
+    private int age;
 }
--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,13 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -92,4 +90,25 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+//    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+//
+//        return "ok";
+//    } //위의 modelAttributeV1과 같은 코드
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+        //@ModelAttribute 생략 가능
+    }
 }
